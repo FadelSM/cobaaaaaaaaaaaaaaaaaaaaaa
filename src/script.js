e6486de370c4383ce9280963e26ae688d03a1d81
FadelSM <rasyahstore8888@gmail.com>
2026-04-27 18:11:10 +0700

    Update script.js

--- src/script.js
+++ src/script.js
@@ -16,7 +16,10 @@ const repository = [
     { id: 15, date: "19 Apr 2026 11:00", name: "Olimpiade Sains dan Teknologi Nasional (OSTN)", host: "Sentral Olimpiade", field: "Biologi", result: "Medali Emas (A+)", link: "#" },
     { id: 16, date: "26 Apr 2026 14:00", name: "Kejuaraan Sains Nasional (KSN)", host: "Puskanas", field: "Informatika", result: "Medali Emas (A+)", link: "#", isAutoRelease: true, releaseDay: 2, releaseHour: 12 },
     { id: 17, date: "24 May 2026 15:00", name: "Sentral Olimpiade Nasional (SEON)", host: "Sentral Olimpiade", field: "Informatika", result: "Coming Soon", link: "#" },
-    { id: 18, date: "24 May 2026 14:00", name: "Sentral Olimpiade Nasional (SEON)", host: "Sentral Olimpiade", field: "Geografi", result: "Coming Soon", link: "#" }
+    { id: 18, date: "24 May 2026 14:00", name: "Sentral Olimpiade Nasional (SEON)", host: "Sentral Olimpiade", field: "Geografi", result: "Coming Soon", link: "#" },
+    { id: 19, date: "28 juny 2026 09:00", name: "Olimpiade Sains Siswa Nasional (OSSN)", host: "Puskanas", field: "Kimia", result: "Coming Soon", link: "#" },
+    { id: 20, date: "28 juny 2026 13:00", name: "Olimpiade Sains Siswa Nasional (OSSN)", host: "Puskanas", field: "Fisika", result: "Coming Soon", link: "#" },
+    { id: 21, date: "28 juny 2026 14:00", name: "Olimpiade Sains Siswa Nasional (OSSN)", host: "Puskanas", field: "Informatika", result: "Coming Soon", link: "#" }
 ];
 
 const renderTarget = document.getElementById('render-target');
